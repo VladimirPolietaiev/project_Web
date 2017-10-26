8b767d33f8d275d6a6f12dce5e97ab8c1d67d021
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-10-27 00:03:12 +0300

    add status item

--- MainPage/Conrollers/MainPageCtrl.js
+++ MainPage/Conrollers/MainPageCtrl.js
@@ -1,8 +1,16 @@
 app.controller("MainPageCtrl", function ($scope,DataService) {
 
     $scope.items = getItemsFromService();
+    $scope.statusItem = anyName;
  
     function getItemsFromService() {
         return DataService.getItems();
+    }
+    
+    function anyName(myFuckingItem) {
+        console.log(myFuckingItem)
+        if (myFuckingItem.Status == 'Declined'){
+            return true;
+        }
     }
 });
